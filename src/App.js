be24219a1ebbfdd216f10abe85200f97e4caec32
ranyo7 <65369571+ranyo7@users.noreply.github.com>
2022-02-04 10:44:23 -0800

    Created React app and deploying it to github pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import Note from './Components/Note';
+import notes from "./notes";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      {notes.map(createNotes)}
+      <Footer/>
     </div>
   );
+}
+
+function createNotes(noteItem){
+  return <Note
+      key={noteItem.key}
+      title={noteItem.title}
+      content={noteItem.content}
+    />
 }
 
 export default App;
